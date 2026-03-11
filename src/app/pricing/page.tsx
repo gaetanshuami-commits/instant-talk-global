0bd8149508151ffaa4a2c19e23f0d6350a16c3dd
Gaetan Shuami <gaetan@email.com>
2026-03-11 16:52:36 +0100

    fix: creation de la page pricing

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,9 @@
+﻿import PricingSection from "@/components/PricingSection";
+
+export default function PricingPage() {
+  return (
+    <main className="min-h-screen bg-gray-50 pt-10">
+      <PricingSection />
+    </main>
+  );
+}
